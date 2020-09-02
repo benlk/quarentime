906b96fa637ce1d511c92707dbcb4ba8f5b3b197
Ben Keith <ben@benlk.com>
2020-09-01 21:30:41 -0400

    Footer notice that this isn't official

--- src/partials/footer.php
+++ src/partials/footer.php
@@ -19,13 +19,7 @@
                             <a class="usa-footer__primary-link" href="https://www.coronavirus.gov/">Coronavirus.gov</a>
                         </li>
                         <li class="mobile-lg:grid-col-6 desktop:grid-col-auto usa-footer__primary-content">
-                            <a class="usa-footer__primary-link" href="javascript:void(0);">Primary link</a>
-                        </li>
-                        <li class="mobile-lg:grid-col-6 desktop:grid-col-auto usa-footer__primary-content">
-                            <a class="usa-footer__primary-link" href="javascript:void(0);">Primary link</a>
-                        </li>
-                        <li class="mobile-lg:grid-col-6 desktop:grid-col-auto usa-footer__primary-content">
-                            <a class="usa-footer__primary-link" href="javascript:void(0);">Primary link</a>
+                            <a class="usa-footer__primary-link" href="https://vote.gov/">Register to vote</a>
                         </li>
                     </ul>
                 </nav>
@@ -35,12 +29,7 @@
                     <div class="grid-row grid-gap">
                         <div class="grid-col-auto mobile-lg:grid-col-12 desktop:grid-col-auto">
                             <div class="usa-footer__contact-info">
-                                <a href="tel:1-800-555-5555">(800) CALL-GOVT</a>
-                            </div>
-                        </div>
-                        <div class="grid-col-auto mobile-lg:grid-col-12 desktop:grid-col-auto">
-                            <div class="usa-footer__contact-info">
-                                <a href="mailto:info@agency.gov">info@agency.gov</a>
+                                <a href="https://projects.propublica.org/represent/">Contact your representatives</a>
                             </div>
                         </div>
                     </div>
@@ -48,14 +37,15 @@
             </div>
         </div>
     </div>
-    <div class="usa-footer__secondary-section">
+    <div class="usa-footer__secondary-section bg-primary-darker text-base-lighter">
         <div class="grid-container">
             <div class="usa-footer__logo grid-row grid-gap-2">
                 <div class="grid-col-auto">
-                    <img class="usa-footer__logo-img" src="assets/img/logo-img.png" alt="">
+                    <img class="usa-footer__logo-img" src="assets/img/loader.gif" alt="">
                 </div>
                 <div class="grid-col-auto">
                     <h3 class="usa-footer__logo-heading">No Official Agency</h3>
+                    <p>This is not an official or unofficial website of the U.S. Government</p>
                 </div>
             </div>
         </div>
